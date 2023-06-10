6363fe54d205a2ad9d4a4d2ce1df1ea033d2f054
Matteus Isaque <matteus.isaque28@gmail.com>
2023-06-10 15:28:58 -0300

    alt

--- src/app/wordpress/page.tsx
+++ src/app/wordpress/page.tsx
@@ -34,73 +34,16 @@ export default function Page() {
           {
             "@context": "https://schema.org/",
             "@type": "Recipe",
-            "name": "Non-alcoholic Pina Colada",
+            "name": "Da Roça Biscoitos",
             "image": [
             "https://example.com/photos/1x1/photo.jpg",
             "https://example.com/photos/4x3/photo.jpg",
-            "https://fastly.picsum.photos/id/237/1920/1080.jpg?hmac=1hPzsubx1j8fMddzUgP5NbuX2rNLOEaQML1rD_g1C5Y"
+            "https://imgur.com/a/Ga5S7Y0"
             ],
             "author": {
-              "@type": "Person",
-              "name": "Mary Stone"
+              "@type": "Da Roça Biscoitos",
+              "name": "Site em wordpress"
             },
-            "datePublished": "2018-03-10",
-            "description": "This non-alcoholic pina colada is everyone's favorite!",
-            "recipeCuisine": "American",
-            "prepTime": "PT1M",
-            "cookTime": "PT2M",
-            "totalTime": "PT3M",
-            "keywords": "non-alcoholic",
-            "recipeYield": "1 serving",
-            "recipeCategory": "Drink",
-            "nutrition": {
-              "@type": "NutritionInformation",
-              "calories": "120 calories"
-            },
-            "aggregateRating": {
-              "@type": "AggregateRating",
-              "ratingValue": "5",
-              "ratingCount": "18"
-            },
-            "recipeIngredient": [
-              "2 cups of pineapple juice",
-              "5/8 cup cream of coconut",
-              "ice"
-            ],
-            "recipeInstructions": [
-              {
-                "@type": "HowToStep",
-                "text": "Blend 2 cups of pineapple juice and 5/8 cup cream of coconut until smooth."
-              },
-              {
-                "@type": "HowToStep",
-                "text": "Fill a glass with ice."
-              },
-              {
-                "@type": "HowToStep",
-                "text": "Pour the pineapple juice and coconut mixture over ice."
-              }
-            ],
-            "video": {
-              "@type": "VideoObject",
-              "name": "How to make a Party Coffee Cake",
-              "description": "This is how you make a Party Coffee Cake.",
-              "thumbnailUrl": [
-                "https://example.com/photos/1x1/photo.jpg",
-                "https://example.com/photos/4x3/photo.jpg",
-                "https://example.com/photos/16x9/photo.jpg"
-               ],
-              "contentUrl": "https://www.example.com/video123.mp4",
-              "embedUrl": "https://www.example.com/videoplayer?video=123",
-              "uploadDate": "2018-02-05T08:00:00+08:00",
-              "duration": "PT1M33S",
-              "interactionStatistic": {
-                "@type": "InteractionCounter",
-                "interactionType": { "@type": "WatchAction" },
-                "userInteractionCount": 2347
-              },
-              "expires": "2019-02-05T08:00:00+08:00"
-             }
           }
           </script>
           `,
