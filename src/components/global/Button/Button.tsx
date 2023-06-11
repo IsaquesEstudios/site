b90c354afdeaf2a0dca229cec5bbcc5f2f2b1e2e
Matteus Isaque <matteus.isaque28@gmail.com>
2023-06-11 12:37:37 -0300

    alt

--- src/components/global/Button/Button.tsx
+++ src/components/global/Button/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 export default function Button({ className, children }: ButtonProps) {
   return (
     <button
-      className={`bg-yellow-800 py-3 rounded-md font-medium text-xl transition hover:bg-yellow-900 ${className}`}
+      className={`bg-yellow-800 py-3 text-black-700 rounded-md font-medium text-xl transition hover:bg-yellow-900 ${className}`}
     >
       {children}
     </button>
