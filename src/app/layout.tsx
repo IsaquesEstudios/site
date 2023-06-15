b5fd9fb48e310b98efc9fa2affc604cc271e7104
Matteus Isaque <matteus.isaque28@gmail.com>
2023-06-15 13:46:55 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import Nav from "@/components/global/Nav/Nav";
 import "./globals.css";
 import Footer from "@/components/global/Footer/Footer";
+import Script from "next/script";
 
 // import Loading from './loading'
 
@@ -13,15 +14,6 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-TW592H4');`,
-          }}
-        />
         <meta
           name="google-site-verification"
           content="M2h9R8mpdIWjAVLPfoTGiyrNS-vS9EKA9AUomMgWjFs"
@@ -31,6 +23,17 @@ export default function RootLayout({
         <Nav />
         {children}
         <Footer />
+        <Script
+          // <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TW592H4');`,
+          }}
+          // />
+        />
       </body>
     </html>
   );
