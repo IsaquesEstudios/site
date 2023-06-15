4bb2ae5948e89b1ddec5b1cce2290cac9de1c476
Matteus Isaque <matteus.isaque28@gmail.com>
2023-06-15 13:31:14 -0300

    alt

--- src/app/wordpress/page.tsx
+++ src/app/wordpress/page.tsx
@@ -25,40 +25,7 @@ import Head from "next/head";
 
 export default function Page() {
   return (
-    <>
       <main>
-        <Head>
-          <title>Sites em wordpress - Tenha seu site pronto em wordpress</title>
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: `
-          {
-            "@context": "https://schema.org/",
-            "@type": "Recipe",
-            "name": "Da Roça Biscoitos",
-            "image": [
-            "https://example.com/photos/1x1/photo.jpg",
-            "https://example.com/photos/4x3/photo.jpg",
-            "https://imgur.com/a/Ga5S7Y0"
-            ],
-            "author": {
-              "@type": "Da Roça Biscoitos",
-              "name": "Site em wordpress"
-            },
-          }
-          </script>
-          `,
-            }}
-          ></script>
-          <meta
-            name="description"
-            content="Sites em wordpress, landing pages, e-commerce, blog e muito mais, profissionais em wordpress estão prontos para apresentar os valores"
-          />
-
-          <link rel="canonical" href="https://isaquesestudios.com/wordpress" />
-        </Head>
-        
         <div
           className="w-full min-h-[1200px] bg-center bg-no-repeat bg-cover flex items-center justify-center mobile:min-h-[900px]"
           style={{ backgroundImage: `url('${Bg.src}')` }}
@@ -235,6 +202,5 @@ export default function Page() {
           </div>
         </div>
       </main>
-    </>
   );
 }
