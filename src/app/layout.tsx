726e5437fed144a461b562fcaf97dd7b7ba31498
Matteus Isaque <matteus.isaque28@gmail.com>
2023-06-21 11:16:50 -0300

    alt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,15 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TW592H4');`,
+          }}
+        />
 
         <meta
           name="google-site-verification"
@@ -27,12 +36,6 @@ export default function RootLayout({
       </head>
       <body>
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TW592H4"
-            height="0"
-            width="0"
-            className="hidden"
-          ></iframe>
         </noscript>
         <Nav />
         {children}
